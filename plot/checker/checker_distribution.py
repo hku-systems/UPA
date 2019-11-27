ca946d78333dbe37c0390c232df25a4123ab584e
Li Tsz On <u3518490@connect.hku.hk>
2019-11-27 20:18:22 +0800

    plot distribution

--- plot/checker/checker_distribution.py
+++ plot/checker/checker_distribution.py
@@ -2,24 +2,27 @@ import random
 import numpy as np
 import sys
 
-#python checker_distribution.py 2 1
+#python checker_distribution.py 2 1: 2 differing records
 
 num_p = int(sys.argv[1])
 dist = int(sys.argv[2])
-p = np.zeros(num_p)
 slow_path = 0
-num_neighbour = 200 #total number of neighbouring records
+num_neighbour = int(sys.argv[3]) #total number of neighbouring records
 num_data = 1600000000 #total number of data
 interval = 1600000000 // num_p
-for j in range(0,700): #700 rounds experiments
+for j in range(0,100): #700 rounds experiments
+    p = np.zeros(num_p)
     for i in range(0,num_neighbour):
         new_index = random.randint(0,num_data) // interval #the random number indicate
         p[new_index] = p[new_index] + 1
 
-    redun_count = [ 1 if p[new_index] > 0  else 0 for i in range(0,num_p)]
+    print(p)
+    redun_count = [ 1 if p[new_index] == 0  else 0 for i in range(0,num_p)]
     redun_count_sum = sum(redun_count)
-    if redun_count_sum <= dist + 1:
+    # print(redun_count_sum)
+    if redun_count_sum < dist + 1:
         slow_path = slow_path + 1
+# print(str(dist + 1))
 print(slow_path)
 
 
