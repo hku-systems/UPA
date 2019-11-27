08a95fd3e4a496b77b1204af2effd0a3ad53f821
Li Tsz On <u3518490@connect.hku.hk>
2019-11-27 17:00:11 +0800

    plot checker distribution

--- plot/checker/checker_distribution.py
+++ plot/checker/checker_distribution.py
@@ -2,15 +2,18 @@ import random
 import numpy as np
 import sys
 
+#python checker_distribution.py 2 1
+
 num_p = int(sys.argv[1])
 dist = int(sys.argv[2])
 p = np.zeros(num_p)
 slow_path = 0
 num_neighbour = 200 #total number of neighbouring records
 num_data = 1600000000 #total number of data
+interval = 1600000000 // num_p
 for j in range(0,700): #700 rounds experiments
     for i in range(0,num_neighbour):
-        new_index = random.randint(num_data) // num_p #the random number indicate
+        new_index = random.randint(0,num_data) // interval #the random number indicate
         p[new_index] = p[new_index] + 1
 
     redun_count = [ 1 if p[new_index] > 0  else 0 for i in range(0,num_p)]
